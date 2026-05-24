c3c5053a2c763ec5d689d8f0fa240556eb049be9
K Santhosh Kumar <codewithsanthosh.official@gmail.com>
2026-05-24 15:08:40 +0530

    PwdAuth_RegsterLogin

--- 7_RegisterLogin/RegMain.py
+++ 7_RegisterLogin/RegMain.py
@@ -4,9 +4,8 @@ from contextlib import asynccontextmanager
 from typing import Annotated
 from pathlib import Path
 from RegModel import User, CreateUser, LoginUser
-from pathlib import Path
-from FileModel import User, CreateUser
 import os, shutil
+import bcrypt
 
 
 
@@ -47,8 +46,58 @@ def get_session():
 SessionDep =  Annotated[Session, Depends(get_session)]
 
 
+# ================================================================ Auth
+
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+
+# def hash_password(password:str) -> str :
+#     return pwd_context.hash(password)
+
+# def verify_password(plain:str, hashed:str) -> bool :
+#     return pwd_context.verify(plain, hashed)
+
+def hash_password(password: str) -> str:
+    pwd_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd_bytes, salt).decode("utf-8")
+
+def verify_password(plain: str, hashed: str) -> bool:
+    return bcrypt.checkpw(plain.encode("utf-8"), hashed.encode("utf-8"))
 
 
+# ================================================================
 
 
+@app.post("/register")
+def register(session: SessionDep, user_data: CreateUser):
+    # Check if user already exists
+    existing_user = session.exec(select(User).where(User.email == user_data.email)).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    
+    hash_pwd = hash_password(user_data.password)
+
+    # Create new user
+    new_user = User(email = user_data.email, name = user_data.name, hashed_password = hash_pwd)
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    return {"message": "User registered successfully", "user_id": new_user}
+    # return {"message": "User registered successfully", "user_id": new_user.id}
+
+
+@app.post("/login")
+def login(session: SessionDep, login_user: LoginUser):
+
+    user = session.exec( select(User).where(User.email == login_user.email)).first()
+
+    if not user:
+        raise HTTPException(status_code=400, detail="Invalid Credentials")
+
+    pwd = verify_password( login_user.password, user.hashed_password)
+
+    if not pwd:
+        raise HTTPException( status_code=400, detail="Invalid Credentials")
+
+    return user
 
